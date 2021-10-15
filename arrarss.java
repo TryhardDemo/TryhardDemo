f24c18ad2d90dc67de1be66db039c7e110fec3a4
TryhardDemo <92570313+TryhardDemo@users.noreply.github.com>
2021-10-15 15:27:57 +0800

    Add files via upload

---
+++ arrarss.java
@@ -0,0 +1,17 @@
+package projectarrayss;
+
+public class arrarss {
+
+	public static void main(String[] args) {
+		System.out.println("arrays ");
+		
+		String bigboy[] = {"can","i","put"};
+		
+		System.out.println("balls!");
+		System.out.println(bigboy[0]);
+		System.out.println(bigboy[1]);
+		System.out.println(bigboy[2]);
+
+	}
+
+}
